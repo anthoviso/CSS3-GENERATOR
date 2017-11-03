ded803721c11846b8b5ae15eb06836564b5a8574
anthoviso <anthonyvisocchi@gmail.com>
2017-11-03 13:15:47 +0100

    Some changes
    js changes.

--- js/principalesFonctions.js
+++ js/principalesFonctions.js
@@ -1,196 +1,163 @@
+/* Javascript PrincipalesFonctions.js */
+var list = ['Width','Height','Color','FontFamily','FontSize','LineHeight','FontStyle','FontWeight','TextDecoration','TextAlign','Background',
+'Opacity','Border','BorderRadius','TextShadow','BoxShadow','Padding','Margin','Display','Visibility','Outline','BoxSizing'];
+
+  for (i=0;i < list.length;i++){
+	 $('.ace_first').after('<div class="ace_line " id="divrender' + list[i] + '" ><p class="ace_gutter ace_gutter-cell" unselectable="on"></p><span id="cssrender' + list[i] + '" class="cssrender' + list[i] + '"></span></div>'); 
+	// console.log(list[i]);
+	}
 
 
 var elementZ;
 $(".menu").click(function(){
-  elementZ = 	$(this).parent().attr('id');
+	elementZ = 	$(this).parent().attr('id');
     if ($("#" + elementZ + " > div").hasClass("active")){
         $("#" + elementZ + " > p > .down").removeClass("activeDown");
         $("#" + elementZ + " > div").removeClass("active");
-    }else{
+		}else{
         $("#" + elementZ + " > p > .down").addClass("activeDown");
         $("#" + elementZ + " > div").addClass("active");
-    }
-    });
+	}
+});
 /* Lorsque l'on clique sur une class on recupere l'id du meme élément et on affiche la class qui pourte la meme valeur que cet id*/
-  var elementP;
-  $(".propertyproperty").click(function(){
-  elementP = $(this).attr('id');
-      $(".synthaxe > div").removeClass("activeProperty");
-      $("." + elementP).addClass("activeProperty");
-  });
+var elementP;
+$(".propertyproperty").click(function(){
+	elementP = $(this).attr('id');
+	$(".synthaxe > div").removeClass("activeProperty");
+	$("." + elementP).addClass("activeProperty");
+});
 
 
 var RegleContainer = document.getElementById("rPixelHaut");
 RegleContainer.innerHTML += "<div class='InvPix10'></div>";
 for (var i = 0; i < 15; i++) {
-RegleContainer.innerHTML += "<div class='GrandPix10'></div>";
-for (var j = 0; j < 9; j++) {
-RegleContainer.innerHTML += "<div class='Pix10'></div>";
-}
+	RegleContainer.innerHTML += "<div class='GrandPix10'></div>";
+	for (var j = 0; j < 9; j++) {
+		RegleContainer.innerHTML += "<div class='Pix10'></div>";
+	}
 }
 
 var RegleContainer2 = document.getElementById("rPixelGauche");
 RegleContainer.innerHTML += "<div class='VInvPix10'></div>";
 for (var i = 0; i < 6; i++) {
-  RegleContainer.innerHTML += "<div class='VGrandPix10'></div>";
-for (var j = 0; j < 9; j++) {
-  RegleContainer.innerHTML += "<div class='VPix10'></div>";
-}
+	RegleContainer.innerHTML += "<div class='VGrandPix10'></div>";
+	for (var j = 0; j < 9; j++) {
+		RegleContainer.innerHTML += "<div class='VPix10'></div>";
+	}
 }
 
-  function activecodehtml () {
-     $('#inputHtml').addClass("activeCode");
-      $('#inputCSS').removeClass("activeCode");
-    $('#inputCSS').parent().removeClass("activeCode");
+function activecodehtml () {
+	$('#inputHtml').addClass("activeCode");
+	$('.inputCSS').removeClass("activeCode");
+    $('.inputCSS').parent().removeClass("activeCode");
     $('#CSSRENDER').hide();
     $('#HTMLRENDER').show();
     $('#copyCodeCSS').hide();
     $('#copyCodeHTML').show();
-   }
+}
 
-   function activecodecss () {
-     $('#inputHtml').removeClass("activeCode");
-    $('#inputCSS').addClass("activeCode");
-    $('#inputCSS').parent().addClass("activeCode");
+function activecodecss () {
+	$('#inputHtml').removeClass("activeCode");
+    $('.inputCSS').addClass("activeCode");
+    $('.inputCSS').parent().addClass("activeCode");
     $('#HTMLRENDER').hide();
     $('#CSSRENDER').show();
-$('#copyCodeCSS').show();
-$('#copyCodeHTML').hide();
-    }
-
-  $(function() {
-$('.range').on('input', function() {
-borderRadius.value = $(this).val() + "px";
-});
-});
+	$('#copyCodeCSS').show();
+	$('#copyCodeHTML').hide();
+}
 
 $(function() {
-$('.range2').on('input', function() {
-opacity.value = $(this).val() / 100;
-});
-});
-
-$(function() {
-$('.range3').on('input', function() {
-border.value = $(this).val() + "px solid black";
-});
-});
-
-
-$(function() {
-$('.range4').on('input', function() {
-padding.value = $(this).val() + "px";
-});
+	$('input').on('input', function() {
+		if($(this).attr('id') == 'rangeType'){
+			$('.' + $(this)[0].className + 'InputLeft')[0].value = $(this).val() + "px";
+		}else if($(this).hasClass('range2')){
+		console.log('sdfsdf');
+			$('#Opacity').val($('.range2').val() / 100);
+		}else if($(this).hasClass('range3')){
+			$('#Border').val($('.range3').val() + "px solid black");
+		}
+	});
 });
 
-$(function() {
-$('.range5').on('input', function() {
-margin.value = $(this).val() + "px";
-});
-});
-
-$(function() {
-$('.range6').on('input', function() {
-FontSize.value = $(this).val() + "px";
-});
-});
-
-$(function() {
-$('.range7').on('input', function() {
-width.value = $(this).val() + "px";
-});
-});
-
-$(function() {
-$('.range8').on('input', function() {
-height.value = $(this).val() + "px";
-});
-});
-  $(function() {
-  $('.range9').on('input', function() {
-  lineHeight.value = $(this).val() + "px";
-  });
-  });
 
 function OnSelectionChange (select) {
-   var selectedOption = select.options[select.selectedIndex];
-   if (selectedOption.value == 'Arial'){
-     FontFamily.value='arial';
-   }else if (selectedOption.value == 'Comic Sans MS'){
-    FontFamily.value='Comic Sans MS';
-  }else if(selectedOption.value == 'Georgia'){
-   FontFamily.value='Georgia';
-  }else if(selectedOption.value == 'Impact'){
-   FontFamily.value='Impact';
-  }else if(selectedOption.value == 'Times New Roman'){
-   FontFamily.value='Times New Roman';
-  }else if(selectedOption.value == 'Verdana'){
-   FontFamily.value='Verdana';
-  }else if(selectedOption.value == ''){
-   FontFamily.value='';
-  }
+	var selectedOption = select.options[select.selectedIndex];
+	if (selectedOption.value == 'Arial'){
+		FontFamily.value='arial';
+		}else if (selectedOption.value == 'Comic Sans MS'){
+		FontFamily.value='Comic Sans MS';
+		}else if(selectedOption.value == 'Georgia'){
+		FontFamily.value='Georgia';
+		}else if(selectedOption.value == 'Impact'){
+		FontFamily.value='Impact';
+		}else if(selectedOption.value == 'Times New Roman'){
+		FontFamily.value='Times New Roman';
+		}else if(selectedOption.value == 'Verdana'){
+		FontFamily.value='Verdana';
+		}else if(selectedOption.value == ''){
+		FontFamily.value='';
+	}
 }
 
 function OnSelectionChange2 (select) {
     var selectedOption2 = select.options[select.selectedIndex];
     if (selectedOption2.value == '10'){
-      FontSize.value='10px';
-    }else if (selectedOption2.value == '12'){
-     FontSize.value='12px';
-   }else if(selectedOption2.value == '16'){
-    FontSize.value='16px';
-  }else if(selectedOption2.value == '20'){
-    FontSize.value='20px';
-  }else if(selectedOption2.value == '24'){
-    FontSize.value='24px';
-  }else if(selectedOption2.value == '30'){
-    FontSize.value='30px';
-   }else if(selectedOption2.value == ''){
-     FontSize.value='';
-    }
+		FontSize.value='10px';
+		}else if (selectedOption2.value == '12'){
+		FontSize.value='12px';
+		}else if(selectedOption2.value == '16'){
+		FontSize.value='16px';
+		}else if(selectedOption2.value == '20'){
+		FontSize.value='20px';
+		}else if(selectedOption2.value == '24'){
+		FontSize.value='24px';
+		}else if(selectedOption2.value == '30'){
+		FontSize.value='30px';
+		}else if(selectedOption2.value == ''){
+		FontSize.value='';
+	}
 }
 /*switch principe*/
 
 function selectText(containerid) {
-  if (document.selection) {
-      var range = document.body.createTextRange();
-      range.moveToElementText(document.getElementById(containerid));
-      range.select();
-  } else if (window.getSelection) {
-      var range = document.createRange();
-      range.selectNode(document.getElementById(containerid));
-      window.getSelection().addRange(range);
-  }
+	if (document.selection) {
+		var range = document.body.createTextRange();
+		range.moveToElementText(document.getElementById(containerid));
+		range.select();
+		} else if (window.getSelection) {
+		var range = document.createRange();
+		range.selectNode(document.getElementById(containerid));
+		window.getSelection().addRange(range);
+	}
 }
 
 
 function reset () {
-txtValue.value = '';
-background.value = "white";
-TextDecoration.value = "";
-FontWeight.value = "";
-color.value = "";
-FontFamily.value = "";
-FontSize.value = "";
-FontStyle.value = "";
-TextAlign.value = "";
-width.value = "100px";
-height.value = "100px";
-border.value = "";
-borderRadius.value = "";
-opacity.value = "";
-boxShadow.value = "";
-textShadow.value = "";
-padding.value = "";
-margin.value = "";
-zindex.value = "";
-boxSizing.value = "";
-position.value = "";
-display.value = "";
-visibility.value = "";
-outline.value = "";
-boxSizing.value = "";
-$(".v-buttonGroupControl").prop("checked", false);
-textValueForm.value="";
-lineHeight.value = "";
+	$('#Background').val("white");
+	$('#TextDecoration').val("");
+	$('#FontWeight').val("");
+	$('#Color').val("");
+	$('#FontFamily').val("");
+	$('#FontSize').val("")
+	$('#FontStyle').val("");
+	$('#TextAlign').val("");
+	$('#Width').val("100px");
+	$('#Height').val("100px");
+	$('#Border').val("");
+	$('#BorderRadius').val("");
+	$('#Opacity').val("");
+	$('#BoxShadow').val("");
+	$('#TextShadow').val("");
+	$('#Padding').val("");
+	$('#Margin').val("");
+	$('#Zindex').val("");
+	$('#BoxSizing').val("");
+	$('#Position').val("");
+	$('#Display').val("");
+	$('#Visibility').val("");
+	$('#Outline').val("");
+	$('#BoxSizing').val("");
+	$(".v-buttonGroupControl").prop("checked", false);
+	textValueForm.value="";
+	$('#LineHeight').val("");
 }
--- js/project.js
+++ js/project.js
@@ -1,234 +1,106 @@
-  //Polyfill for requestAnimationFrame
-  window.requestAnimationFrame = (function(){
+//Polyfill for requestAnimationFrame
+window.requestAnimationFrame = (function(){
     return  window.requestAnimationFrame       ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame    ||
     window.oRequestAnimationFrame      ||
     window.msRequestAnimationFrame     ||
     function(/* function */ callback, /* DOMElement */ element){
-      window.setTimeout(callback, 1000 / 60);
-    };
-  })();
+		window.setTimeout(callback, 1000 / 60);
+	};
+})();
 
-  //Get a reference to the input and output
+// var container = document.getElementById("output");
+var container2 = document.getElementById("output2");
+var container3 = document.getElementById("htmlTxtValue");
+var colorValue = $('#color').val();
+var dieze = '';
+var listValues = ['Width','Height','Color','FontFamily','FontSize','LineHeight','FontStyle','FontWeight','TextDecoration','TextAlign','Background',
+	'Opacity','Border','BorderRadius','TextShadow','BoxShadow','Padding','Margin','Display','Visibility','Outline','BoxSizing'];
 
-  var txtValue = document.getElementById("txtValue");
-
-  var color = document.getElementById("color");
-  var FontFamily = document.getElementById("FontFamily");
-  var FontSize = document.getElementById("FontSize");
-  var FontStyle = document.getElementById("FontStyle");
-  var TextAlign = document.getElementById("TextAlign");
-  var FontWeight = document.getElementById("FontWeight");
-  var TextDecoration = document.getElementById("TextDecoration");
-  var width = document.getElementById("width");
-  var height = document.getElementById("height");
-  var background = document.getElementById("background");
-
-  var border = document.getElementById("border");
-  var borderRadius = document.getElementById("borderRadius");
-  var opacity = document.getElementById("opacity");
-  var boxShadow = document.getElementById("boxShadow");
-  var textShadow = document.getElementById("textShadow");
-
-  var padding = document.getElementById("padding");
-  var margin = document.getElementById("margin");
-  var zindex = document.getElementById("zindex");
-  var boxSizing = document.getElementById("boxSizing");
-  var position = document.getElementById("position");
-
-
-  var boxSizing = document.getElementById("boxSizing");
-  var display = document.getElementById("display");
-  var visibility = document.getElementById("visibility");
-  var outline = document.getElementById("outline");
-
-  var container = document.getElementById("output");
-  var container2 = document.getElementById("output2");
-  var container3 = document.getElementById("htmlTxtValue");
-  var colorValue = $('#color').val();
-  var dieze = '';
-
-  //Set up a requestAnimationFrame loop
-  function update () {
+	
+//Set up a requestAnimationFrame loop
+function update () {
     requestAnimationFrame(update);
-
-    container3.innerHTML = txtValue.value;
-    container.innerHTML = " <div id='outputContainer' style=\"background: " + dieze + background.value +
-    ";color: " + dieze + color.value +
-    ";font-family: " + FontFamily.value +
-    ";box-sizing: " + boxSizing.value +
-    ";line-height: " + lineHeight.value +
-    ";display: " + display.value +
-    ";visibility: " + visibility.value +
-    ";outline: " + outline.value +
-    ";font-size: " + FontSize.value +
-    ";font-style: " + FontStyle.value +
-    ";text-align: " + TextAlign.value +
-    ";font-weight: " + FontWeight.value +
-    ";text-decoration: " + TextDecoration.value +
-    ";width: " + width.value +
-    ";height : " + height.value +
-    ";border : " + border.value +
-    ";border-radius : " + borderRadius.value +
-    ";opacity : " + opacity.value +
-    ";box-shadow : " + boxShadow.value +
-    ";text-shadow : " + textShadow.value +
-    ";padding : " + padding.value +
-    ";margin : " + margin.value +
-    ";z-index : " + zindex.value +
-    ";box-sizing : " + boxSizing.value +
-    ";position : " + position.value + ";\" >" + txtValue.value  + "</div>"
+	
+    container3.innerHTML = textValueForm.value;
+	$('#outputContainer').css({"background": "" + dieze + $('#Background')[0].value + "",
+		"color":"" + dieze + $('#Color')[0].value + "",
+		"font-family":"" + $('#FontFamily')[0].value + "",
+		"box-sizing":"" + $('#BoxSizing')[0].value + "",
+		"line-height":"" + $('#LineHeight')[0].value + "",
+		"display":"" + $('#Display')[0].value + "",
+		"visibility":"" + $('#Visibility')[0].value + "",
+		"outline":"" + $('#Outline')[0].value + "",
+		"font-size":"" + $('#FontSize')[0].value + "",
+		"font-style":"" + $('#FontStyle')[0].value + "",
+		"text-align":"" + $('#TextAlign')[0].value + "",
+		"font-weight":"" + $('#FontWeight')[0].value + "",
+		"text-decoration":"" + $('#TextDecoration')[0].value + "",
+		"width":"" + $('#Width')[0].value + "",
+		"height":"" + $('#Height')[0].value + "",
+		"border":"" + $('#Border')[0].value + "",
+		"border-radius":"" + $('#BorderRadius')[0].value + "",
+		"opacity":"" + $('#Opacity')[0].value + "",
+		"box-shadow":"" + $('#BoxShadow')[0].value + "",
+		"text-shadow":"" + $('#TextShadow')[0].value + "",
+		"padding":"" + $('#Padding')[0].value + "",
+		"margin":""  + $('#Margin')[0].value + "",
+		"z-index":"" + $('#Zindex')[0].value + "",
+		"box-sizing":"" + $('#BoxSizing')[0].value + "",
+		"position":"" + $('#Position')[0].value + ";\" >" + textValueForm.value  + ""
+		});
+		$('#outputContainer').html(textValueForm.value);
 
     /* generate code*/
-if(width.value == ""){
-  divrenderWidth.style.display="none";
-}else{
-  divrenderWidth.style.display="block";
-  cssrenderWidth.innerHTML = "<span class='attributs'>width</span> : " + width.value + ";";
-}
 
-if(lineHeight.value == ""){
-  divrenderlineHeight.style.display="none";
-}else{
-  divrenderlineHeight.style.display="block";
-  cssrenderlineHeight.innerHTML = "<span class='attributs'>line-height</span> : " + lineHeight.value + ";";
-}
+	for (i=0;i < listValues.length;i++){
+		if($('#' + listValues[i]).val() == ""){
+			$('.divrender' + listValues[i]).css('display','none');
+			}
+		else{
+			$('.divrender' + listValues[i]).css('display','block');
+			$('.cssrender' + listValues[i]).html("<span class='attributs'>" + listValues[i] + "</span> : " + $('#' + listValues[i]).val() + ";");
+		}		
+	}
 
-if(boxSizing.value == ""){
-  divrenderBoxSizing.style.display="none";
-}else{
-  divrenderBoxSizing.style.display="block";
-  cssrenderBoxSizing.innerHTML = "<span class='attributs'>box-sizing</span> : " + boxSizing.value + ";";
+	/*
+		if ( color.value.length == 6){
+		dieze = "#";
+		}else{dieze = "";}
+	*/
 }
-if(display.value == ""){
-  divrenderDisplay.style.display="none";
-}else{
-  divrenderDisplay.style.display="block";
-  cssrenderDisplay.innerHTML = "<span class='attributs'>display</span> : " + display.value + ";";
-}
+update();
 
-
-if(visibility.value == ""){
-  divrenderVisibility.style.display="none";
-}else{
-  divrenderVisibility.style.display="block";
-  cssrenderVisibility.innerHTML = "<span class='attributs'>visibility</span> : " + visibility.value + ";";
-}
-if(outline.value == ""){
-  divrenderOutline.style.display="none";
-}else{
-  divrenderOutline.style.display="block";
-  cssrenderOutline.innerHTML = "<span class='attributs'>outline</span> : " + outline.value + ";";
-}
+// $('body').on('mousedown','mouseup','keyup', function() {
+	// console.log("event");
+	// });
 
 
 
-
-if(height.value == ""){
-  divrenderHeight.style.display="none";
-}else{
-  divrenderHeight.style.display="block";
-  cssrenderHeight.innerHTML = "<span class='attributs'>height</span> : " + height.value + ";";
-}
-if(color.value == ""){
-  divrenderColor.style.display="none";
-}else{
-  divrenderColor.style.display="block";
-  cssrenderColor.innerHTML = "<span class='attributs'>color</span> : " + dieze + color.value + ";";
-}
-if(FontFamily.value == ""){
-  divrenderFontFamily.style.display="none";
-}else{
-  divrenderFontFamily.style.display="block";
-  cssrenderFontFamily.innerHTML = "<span class='attributs'>font-family</span> : " + FontFamily.value + ";";
-}
-if(FontSize.value == ""){
-  divrenderFontSize.style.display="none";
-}else{
-  divrenderFontSize.style.display="block";
-  cssrenderFontSize.innerHTML = "<span class='attributs'>font-size</span> : " + FontSize.value + ";";
-}
-
-if(FontStyle.value == ""){
-  divrenderFontStyle.style.display="none";
-}else{
-  divrenderFontStyle.style.display="block";
-  cssrenderFontStyle.innerHTML = "<span class='attributs'>font-style</span> : " + FontStyle.value + ";";
-}
-if(FontWeight.value == ""){
-  divrenderFontWeight.style.display="none";
-}else{
-  divrenderFontWeight.style.display="block";
-  cssrenderFontWeight.innerHTML = "<span class='attributs'>font-weight</span> : " + FontWeight.value + ";";
-}
-if(TextDecoration.value == ""){
-  divrenderTextDecoration.style.display="none";
-}else{
-  divrenderTextDecoration.style.display="block";
-  cssrenderTextDecoration.innerHTML = "<span class='attributs'>text-decoration</span> : " + TextDecoration.value + ";";
-}
-
-
-
-if(TextAlign.value == ""){
-  divrenderTextAlign.style.display="none";
-}else{
-  divrenderTextAlign.style.display="block";
-  cssrenderTextAlign.innerHTML = "<span class='attributs'>text-align</span> : " + TextAlign.value + ";";
-}
-if(background.value == ""){
-  divrenderBackground.style.display="none";
-}else{
-  divrenderBackground.style.display="block";
-  cssrenderBackground.innerHTML = "<span class='attributs'>background</span> : " + dieze + background.value + ";";
-}
-if(border.value == ""){
-  divrenderBorder.style.display="none";
-}else{
-  divrenderBorder.style.display="block";
-  cssrenderBorder.innerHTML = "<span class='attributs'>border</span> : " + border.value + ";";
-}
-if(borderRadius.value == ""){
-  divrenderBorderRadius.style.display="none";
-}else{
-  divrenderBorderRadius.style.display="block";
-  cssrenderBorderRadius.innerHTML = "<span class='attributs'>border-radius</span> : " + borderRadius.value + ";";
-}
-if(opacity.value == ""){
-  divrenderOpacity.style.display="none";
-}else{
-  divrenderOpacity.style.display="block";
-  cssrenderOpacity.innerHTML = "<span class='attributs'>opacity</span> : " + opacity.value + ";";
-}
-if(padding.value == ""){
-  divrenderPadding.style.display="none";
-}else{
-  divrenderPadding.style.display="block";
-  cssrenderPadding.innerHTML = "<span class='attributs'>padding</span> : " + padding.value + ";";
-}
-if(margin.value == ""){
-  divrenderMargin.style.display="none";
-}else{
-  divrenderMargin.style.display="block";
-  cssrenderMargin.innerHTML = "<span class='attributs'>margin</span> : " + margin.value + ";";
-}
-if(textShadow.value == ""){
-  divrenderTextShadow.style.display="none";
-}else{
-  divrenderTextShadow.style.display="block";
-  cssrenderTextShadow.innerHTML = "<span class='attributs'>text-Shadow</span> : " + textShadow.value + ";";
-}
-if(boxShadow.value == ""){
-  divrenderBoxShadow.style.display="none";
-}else{
-  divrenderBoxShadow.style.display="block";
-  cssrenderBoxShadow.innerHTML = "<span class='attributs'>box-shadow</span> : " + boxShadow.value + ";";
-}
-/*
-if ( color.value.length == 6){
-    dieze = "#";
-  }else{dieze = "";}
-*/
-  }
-  update();
+    // container.innerHTML = " <div id='outputContainer' style=\"background: " + dieze + $('#Background')[0].value +
+    // ";color: " + dieze + $('#Color')[0].value +
+    // ";font-family: " + $('#FontFamily')[0].value +
+    // ";box-sizing: " + $('#BoxSizing')[0].value +
+    // ";line-height: " + $('#LineHeight')[0].value +
+    // ";display: " + $('#Display')[0].value +
+    // ";visibility: " + $('#Visibility')[0].value +
+    // ";outline: " + $('#Outline')[0].value +
+    // ";font-size: " + $('#FontSize')[0].value +
+    // ";font-style: " + $('#FontStyle')[0].value +
+    // ";text-align: " + $('#TextAlign')[0].value +
+    // ";font-weight: " + $('#FontWeight')[0].value +
+    // ";text-decoration: " + $('#TextDecoration')[0].value +
+    // ";width: " + $('#Width')[0].value +
+    // ";height : " + $('#Height')[0].value +
+    // ";border : " + $('#Border')[0].value +
+    // ";border-radius : " + $('#BorderRadius')[0].value +
+    // ";opacity : " + $('#Opacity')[0].value +
+    // ";box-shadow : " + $('#BoxShadow')[0].value +
+    // ";text-shadow : " + $('#TextShadow')[0].value +
+    // ";padding : " + $('#Padding')[0].value +
+    // ";margin : " + $('#Margin')[0].value +
+    // ";z-index : " + $('#Zindex')[0].value +
+    // ";box-sizing : " + $('#BoxSizing')[0].value +
+    // ";position : " + $('#Position')[0].value + ";\" >" + textValueForm.value  + "</div>"
+	
