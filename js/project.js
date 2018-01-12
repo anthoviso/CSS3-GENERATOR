8d71f4475075b758ae2e14e77f7b1d8633d6d0af
Anthony VISOCCHI <anthonyvisocchi@gmail.com>
2018-01-12 12:13:02 +0100

    V1.3.6

--- js/project.js
+++ js/project.js
@@ -13,14 +13,15 @@ var init = true;
 var unit = 'px';
 var localData =
 {"-defaultClass":{
-  "color" : "#464665",
+  "color" : "#ffbdbd",
   "font-size" : "40px",
   "font-weight" : "bold",
   "text-align" : "center",
+  "text-shadow" : "1px 1px 1px red",
   "margin" : "100px auto"
 },
 "-defaultClass--hover":{
-"color" : "#626569"
+  "color" : "#f2774c"
 }
 };
 
@@ -56,24 +57,6 @@ function button_sortInvByClass(){
   var elemToSort = $('.cssElements').find('section > div').sort(sortInvByClass);
   $('section').append(elemToSort);
 }
-function OnSelectionChange (select) {
-  var selectedOption = select.options[select.selectedIndex];
-  if (selectedOption.value == 'Arial'){
-    $('#font-family').val('Arial');
-  }else if (selectedOption.value == 'Comic Sans MS'){
-    $('#font-family').val('Comic Sans MS');
-  }else if(selectedOption.value == 'Georgia'){
-    $('#font-family').val('Georgia');
-  }else if(selectedOption.value == 'Impact'){
-    $('#font-family').val('Impact');
-  }else if(selectedOption.value == 'Times New Roman'){
-    $('#font-family').val('Times New Roman');
-  }else if(selectedOption.value == 'Verdana'){
-    $('#font-family').val('Verdana');
-  }else if(selectedOption.value == ''){
-    $('#font-family').val('');
-  }
-}
 function selectText(containerid) {
   if (document.selection) {
     var range = document.body.createTextRange();
@@ -86,12 +69,12 @@ function selectText(containerid) {
   }
 }
 function resetHtml(){
-    editor.getDoc().setValue('<div></div>');
+  editor.getDoc().setValue('<div></div>');
 }
 function resetAll () {
   $('#CSSRENDER .classDiv').remove();
   $('#output').empty();
-    $('section input').val("");
+  $('section input').val("");
   // console.log(localData);
   localData = {};
   $(".v-buttonGroupControl").prop("checked", false);
@@ -100,9 +83,9 @@ function resetAll () {
 }
 function delClass () {
   // console.log(selectedClass);
-      $('#CSSRENDER #' + selectedClass).remove();
-    $('.-' + selectedClass).remove();
-    delete localData['-' + selectedClass];
+  $('#CSSRENDER #' + selectedClass).remove();
+  $('.-' + selectedClass).remove();
+  delete localData['-' + selectedClass];
 }
 function resetClass () {
   delete localData['-' + selectedClass];
@@ -122,27 +105,62 @@ function activecodehtml () {
   $('#HTMLRENDER').show();
   $('#copyCodeCSS').hide();
   $('#copyCodeHTML').show();
-if(initCodeMirror == true){
-  //Codemirror
-  editor = CodeMirror(document.getElementById("codemirror-html"), {
-    mode: "xml",
-    extraKeys: {"Ctrl-Space": "autocomplete"},
-    value: "<div class='defaultClass'>CSS3 Generator</div>",
-    lineNumbers: true,
-    enterMode: "keep",
-    autofocus:true,
-    theme: "monokai",
-    indentWithTabs: true
-  });
-  $(editor.getWrapperElement()).slideDown('normal', function(){
-        editor.refresh();
+  if(initCodeMirror == true){
+    //Codemirror
+    editor = CodeMirror(document.getElementById("codemirror-html"), {
+      mode: "xml",
+      extraKeys: {"Ctrl-Space": "autocomplete"},
+      value: "<div class='defaultClass'>CSS3 Generator</div>",
+      lineNumbers: true,
+      enterMode: "keep",
+      autofocus:true,
+      theme: "one-dark",
+      indentWithTabs: true
+    });
+    editor.addKeyMap({
+      "Tab": function (cm) {
+        if (cm.somethingSelected()) {
+          var sel = editor.getSelection("\n");
+          // Indent only if there are multiple lines selected, or if the selection spans a full line
+          if (sel.length > 0 && (sel.indexOf("\n") > -1 || sel.length === cm.getLine(cm.getCursor().line).length)) {
+            cm.indentSelection("add");
+            return;
+          }
+        }
+
+        if (cm.options.indentWithTabs)
+        cm.execCommand("insertTab");
+        else
+        cm.execCommand("insertSoftTab");
+      },
+      "Shift-Tab": function (cm) {
+        cm.indentSelection("subtract");
+      }
+    });
+    $(editor.getWrapperElement()).slideDown('normal', function(){
+      editor.refresh();
     });
     $('.htmlCodeValue').html(editor.getValue());
 
     initCodeMirror = false;
 
-      $('#output').html(editor.getValue().replace(/[cC]lass='/g, "class='-").replace(/[cC]lass="/g, 'class="-'));
+    $('#output').html(editor.getValue().replace(/[cC]lass='/g, "class='-").replace(/[cC]lass="/g, 'class="-'));
   }
+}
+function getSelectedRange() {
+  return { from: editor.getCursor(true), to: editor.getCursor(false) };
+}
+function autoFormatSelection() {
+  var range = getSelectedRange();
+  editor.autoFormatRange(range.from, range.to);
+}
+function commentSelection(isComment) {
+  var range = getSelectedRange();
+  editor.commentRange(isComment, range.from, range.to);
+}
+function indentHtml(){
+  console.log(editor.getDoc().setValue(editor.getValue().replace(/\,/g,', ').replace(/\{/g,' {\n\t').replace(/\}/g,'}\n')));
+  console.log(editor.getValue().replace(/\,/g,', ').replace(/\{/g,' {\n\t').replace(/\}/g,'}\n'));
 }
 function activecodecss () {
   $('#inputHTMLcode').removeClass("activeCode");
@@ -189,7 +207,7 @@ function searchPropterties(){
     if($('#inputFilter').val() != ""){
       $('.cssSearch .searchNResults').text(numElem + " found");
     }else{
-        $('.cssSearch .searchNResults').text("");
+      $('.cssSearch .searchNResults').text("");
     }
   }
 }
@@ -214,20 +232,20 @@ function switchToSpan() {
   var tmpSpan = $span.text().replace(/^\-/,'').replace(/[^A-z\d\_\:\-]/g,'').replace(/\::/,"---").replace(/\:/,"--");
   if(oldSpan != tmpSpan){
     if(tmpSpan == "" || tmpSpan == "undefined"){
-    console.log('tmp vide');
-    $span.val(oldSpan);
+      console.log('tmp vide');
+      $span.val(oldSpan);
       tmpSpan = oldSpan;
       console.log(localData);
     }else{
 
       console.log('tmp rempli');
-            console.log(tmpSpan);
+      console.log(tmpSpan);
 
-        $('.-' + oldSpan).addClass('-' + tmpSpan);
-        $('.-' + oldSpan).removeClass('-' + oldSpan);
-        $('.' + oldSpan).addClass(tmpSpan);
-        $('.' + oldSpan).removeClass(oldSpan);
-        $('#' + oldSpan).attr("id", tmpSpan);
+      $('.-' + oldSpan).addClass('-' + tmpSpan);
+      $('.-' + oldSpan).removeClass('-' + oldSpan);
+      $('.' + oldSpan).addClass(tmpSpan);
+      $('.' + oldSpan).removeClass(oldSpan);
+      $('#' + oldSpan).attr("id", tmpSpan);
 
 
       localData['-' + tmpSpan] = localData['-' + oldSpan];
@@ -241,7 +259,7 @@ function switchToSpan() {
     console.log(localData);
   }
 
-    console.log('tmpSpan : ' + tmpSpan);
+  console.log('tmpSpan : ' + tmpSpan);
   $span.addClass("loadNum");
   $(this).replaceWith('<span style="margin:0;padding-left:5px;" class="loadNum">' + tmpSpan.replace(/\---/,"::").replace(/\--/,":") + '</span>');
   $('#CSSRENDER').on("click", "span.loadNum", switchToInput);
@@ -252,7 +270,7 @@ function selectedClassFunc(thisObj){
     console.log("selectedClass : " + selectedClass);
     $('.classDiv').removeClass('selectedClass');
     $('#' + selectedClass).addClass('selectedClass');
-    $('.spanSelected').html(" : ." + selectedClass.replace(/\---/,"::").replace(/\--/,":"));
+    $('.spanSelected').html("." + selectedClass.replace(/\---/,"::").replace(/\--/,":"));
     $('section input').val('');
     for (i=0;i <  Object.keys(localData['-' + selectedClass]).length;i++){
       $('section #' +  Object.keys(localData['-' + selectedClass])[i]).val(localData['-' + selectedClass][Object.keys(localData['-' + selectedClass])[i]]);
@@ -277,14 +295,14 @@ function update (jsonObj) {
   /* generate code*/
   if(init == true){
 
-      $('section input').val('');
-  for (i=0;i <  Object.keys(localData['-' + selectedClass]).length;i++){
-    $('section #' +  Object.keys(localData['-' + selectedClass])[i]).val(localData['-' + selectedClass][Object.keys(localData['-' + selectedClass])[i]]);
+    $('section input').val('');
+    for (i=0;i <  Object.keys(localData['-' + selectedClass]).length;i++){
+      $('section #' +  Object.keys(localData['-' + selectedClass])[i]).val(localData['-' + selectedClass][Object.keys(localData['-' + selectedClass])[i]]);
+    }
+    $('.classDiv').removeClass('selectedClass');
+    $('#' + selectedClass).addClass('selectedClass');
+    $('.spanSelected').html("." + selectedClass.replace(/\---/,"::").replace(/\--/,":"));
   }
-  $('.classDiv').removeClass('selectedClass');
-  $('#' + selectedClass).addClass('selectedClass');
-  $('.spanSelected').html(" : ." + selectedClass.replace(/\---/,"::").replace(/\--/,":"));
-}
   // console.log('slectedClass : ' + selectedClass );
   var elProperty = jsonObj['properties'];
   if($('.htmlCodeValue > .defaultClass > p').text() == "" && initCodeMirror == true){
@@ -295,22 +313,22 @@ function update (jsonObj) {
     if($('#' + elProperty[i].property).val() == ""){
       delete localData['-' + selectedClass][elProperty[i].property];
     }else{
-      if($('.' + selectedClass + ' #cssrender' + elProperty[i].property).hasClass('tmpRemove')){
+      if($('.' + selectedClass + ' #cssrender' + elProperty[i].property).hasClass('tmpRemove') && localData['-' + selectedClass][elProperty[i].property].indexOf("/*") == -1){
         localData['-' + selectedClass][elProperty[i].property]= "/*" + $('#' + elProperty[i].property).val() + "*/";
       }else{
         localData['-' + selectedClass][elProperty[i].property]= $('#' + elProperty[i].property).val();
       }
     }
   }
-    // Afficher les valeurs dans le code généré
+  // Afficher les valeurs dans le code généré
   for (i=0;i <  Object.keys(localData['-' + selectedClass]).length;i++){
     var elProp = Object.keys(localData['-' + selectedClass])[i];
     var elVal = localData['-' + selectedClass][Object.keys(localData['-' + selectedClass])[i]];
-      // SI valeur input ""
+    // SI valeur input ""
     if($('#' + elProp).val() == ""){
       $('.' + selectedClass + ' #divrender' + elProp).remove();
     }else{
-        // SI NON valeur input ""
+      // SI NON valeur input ""
       if($('.' + selectedClass + ' #divrender' + elProp).length > 0){
       }else{
         $('.' + selectedClass).append('<div class="ace_line " id="divrender' + elProp
@@ -321,23 +339,23 @@ function update (jsonObj) {
     // SI TMP REMOVE
     if($('.' + selectedClass + ' #cssrender' +  elProp).hasClass('tmpRemove')){
       $('.' + selectedClass + ' .cssrender' + elProp).html("<span class='cssCommentStart'>" + cssCommentStart
-        + "</span><span class='attributs'>" + elProp + "</span> : " + $('#' + elProp).val() + "; <span class='cssCommentEnd'>" + cssCommentEnd + "</span>");
+      + "</span><span class='attributs'>" + elProp + "</span> : " + $('#' + elProp).val() + "; <span class='cssCommentEnd'>" + cssCommentEnd + "</span>");
     }else{
       // SI NON TMP REMOVE
-      $('.' + selectedClass + ' .cssrender' + elProp).html("<span class='attributs'>" + elProp + "</span> : " + $('#' + elProp).val() + ";");
+      $('.' + selectedClass + ' .cssrender' + elProp).html("<span class='attributs'>" + elProp + "</span> : " + $('#' + elProp).val().replace('/*','').replace('*/','')  + ";");
     }
   }
 
-// style
+  // style
   $('body style').empty();
-for(s=0;s < Object.keys(localData).length;s++){
-  var styleValue = "";
-  for(e=0;e < Object.keys(localData[Object.keys(localData)[s]]).length;e++){
-  var tttt = Object.keys(localData)[s];
-    styleValue += Object.keys(localData[tttt])[e] + ":" + localData[Object.keys(localData)[s]][Object.keys(localData[tttt])[e]] + ";";
+  for(s=0;s < Object.keys(localData).length;s++){
+    var styleValue = "";
+    for(e=0;e < Object.keys(localData[Object.keys(localData)[s]]).length;e++){
+      var tttt = Object.keys(localData)[s];
+      styleValue += Object.keys(localData[tttt])[e] + ":" + localData[Object.keys(localData)[s]][Object.keys(localData[tttt])[e]] + ";";
     }
-  $('body style').append('.' + Object.keys(localData)[s].replace(/\---/,"::").replace(/\--/,":") + '{' + styleValue + '}');
-}
+    $('body style').append('.' + Object.keys(localData)[s].replace(/\---/,"::").replace(/\--/,":") + '{' + styleValue + '}');
+  }
 
 
   // console.log(localData);
@@ -371,35 +389,27 @@ function showProperties(jsonObj) {
     '" target="_blank"><span "="" class="button_help"><i class="fa fa-question-circle" aria-hidden="true"></i></span></a>' + myinput2Elem + '</div>';
     $('.cssElements > section').append(mydivElem);
 
-if(elProperty[i].values){
-  $( "#" + elProperty[i].property ).autocomplete({
-    source: elProperty[i].values
-  });
-}
-
-
+    if(elProperty[i].values){
+      $( "#" + elProperty[i].property ).autocomplete({
+        source: elProperty[i].values
+      });
+    }
   }
-
-
-
-if(init == true){
-  for(i=0;i<=Object.keys(localData).length - 1;i++){
-    selectedClass = Object.keys(localData)[i].replace('-','');
-    // console.log(selectedClass + " : update des classes en cours");
-    update(jsonObj);
-    toggleEmptyElem();
-    initJsonClass = initJsonClass + 1;
-    if(initJsonClass == Object.keys(localData).length && init == true){  init = false;}
+  if(init == true){
+    for(i=0;i<=Object.keys(localData).length - 1;i++){
+      selectedClass = Object.keys(localData)[i].replace('-','');
+      // console.log(selectedClass + " : update des classes en cours");
+      update(jsonObj);
+      toggleEmptyElem();
+      initJsonClass = initJsonClass + 1;
+      if(initJsonClass == Object.keys(localData).length && init == true){  init = false;}
+    }
   }
-}
-
   $(document).on('load click keyup input', function(){
     update(jsonObj);
     toggleEmptyElem();
   });
-
 }
-
 
 $( document ).ready(function() {
   // JSON
@@ -418,7 +428,10 @@ $( document ).ready(function() {
   $('#CSSRENDER').on('click', '.classDiv', function(){
     selectedClassFunc($(this));
   });
-
+  $('#HTMLRENDER').on('click', '.CodeMirror-code > div', function(){
+    $('.selectedDiv').removeClass('selectedDiv');
+    $(this).addClass('selectedDiv');
+  });
   $('#inputFilter').on('click input', function(){
     searchPropterties();
   });
@@ -434,7 +447,7 @@ $( document ).ready(function() {
   });
   $('#CSSRENDER').on("keyDown keyup", "input.loadNum", function(){
     inputResize();
-});
+  });
 
   $(function() {
     $("#CSSRENDER").on("click", "span.loadNum", switchToInput);
@@ -448,39 +461,6 @@ $( document ).ready(function() {
   $(function() {
     $('.btnToggle').on('click', function() {
       $('.outilsContainer').toggle();
-    });
-    $('#FontWeightBoldForm').on('click', function() {
-      if($('#FontWeightBoldForm').is(':checked')){
-        $('#font-weight').val('bold');
-      }else{
-        $('#font-weight').val('initial');
-      }
-    });
-    $('#FontStyleItalicForm').on('click', function() {
-      if($('#FontStyleItalicForm').is(':checked')){
-        $('#font-style').val('italic');
-      }else{
-        $('#font-style').val('initial');
-      }
-    });
-    $('#TextUnderlineForm').on('click', function() {
-      if($('#TextUnderlineForm').is(':checked')){
-        $('#text-decoration').val('underline');
-      }else{
-        $('#text-decoration').val('none');
-      }
-    });
-    $('#TextAlignLeft').on('click', function() {
-      $('#text-align').val('left');
-    });
-    $('#TextAlignRight').on('click', function() {
-      $('#text-align').val('right');
-    });
-    $('#TextAlignCenter').on('click', function() {
-      $('#text-align').val('center');
-    });
-    $('#TextAlignJustify').on('click', function() {
-      $('#text-align').val('justify');
     });
     $('.backWhite').on('click', function() {
       $('#output').css('background', 'url("img/fondBlanc.png")');
@@ -516,8 +496,8 @@ $( document ).ready(function() {
       var removeParent =   $(this).parent().attr('id');
       var ParentClass = $(this).parent().parent().parent().attr('id');
       removeParent = removeParent.replace('divrender','');
-              // console.log(removeParent);
-              // console.log(ParentClass);
+      // console.log(removeParent);
+      // console.log(ParentClass);
       $('.' + removeParent + 'Proprieties > input').val('');
       // console.log($('.' + removeParent + 'Proprieties > input').val());
       $('.' + ParentClass + '#cssrender' + removeParent).removeClass('tmpRemove');
